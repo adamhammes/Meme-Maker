dc6e7eb9edd2bc97c44b1af18b41e1c5c03c4e91
adamhammes <adamhammes@centetune.com>
2014-03-06 19:02:34 -0600

    Initial commit

---
+++ font_functions.h
@@ -0,0 +1,14 @@
+#include "image_functions.h"
+#include "structs.h"
+#include <string.h>
+
+
+void no_whitespace( char* str );
+Font* read_font_file( char* name );
+void free_font( Font* font );
+
+
+int set_letter( Font* f, char c, int x, int y, int w, int h);
+int get_width( Font* f, char c );
+int calculate_width( Font* font, char* sequece );
+int crop_position( Font* f, Image* pic, char* sequence, int index, int x, int y );
