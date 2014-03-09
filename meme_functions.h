08d3519e44f17e897d9bab859b2cf097c381e700
adamhammes <adamhammes@centetune.com>
2014-03-09 06:13:58 -0500

    Long night of coding...

--- meme_functions.h
+++ meme_functions.h
@@ -1,6 +1,14 @@
 #ifndef _MEME_FUNCTIONS_H_
 #define _MEME_FUNCTIONS_H_
 
+#include "font_functions.h"
+
+Place* get_place( Meme* meme, int index );
+int count_spaces( char* line );
+Meme make_meme( char* name );
+Meme* get_meme( Database* data, char* name );
+void read_meme_file( Database* data, char* name );
+void free_database( Database* data );
 
 
 
